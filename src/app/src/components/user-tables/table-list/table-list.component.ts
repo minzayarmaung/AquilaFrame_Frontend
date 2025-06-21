f78c08ff358e18d19e0092e77e019d30f41b9f55
Min Zayar Maung <minzayarmaung2002@gmail.com>
2025-06-21 19:54:58 +0630

    add

--- src/app/src/components/user-tables/table-list/table-list.component.ts
+++ src/app/src/components/user-tables/table-list/table-list.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { CreateTableComponent } from '../create-table/create-table.component';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { Result } from '../../../../auth/auth.service';
 
 @Component({
   selector: 'app-table-list',
@@ -12,11 +13,17 @@ import { environment } from '../../../environments/environment';
   styleUrl: './table-list.component.css'
 })
 export class TableListComponent {
+isDeleting = false;
+showDeleteModal = false;
+selectedTable: string | null = null;
 private baseUrl = environment.apiBaseUrl;
 tables: string[] = [];
 showCreateModal = false;
 
-constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.fetchTables();
+  }
+
 
 ngOnInit() {
   this.fetchTables();
@@ -32,6 +39,34 @@ fetchTables() {
 onTableCreated() {
   this.showCreateModal = false;
   this.fetchTables(); // Refresh after new table created
+}
+
+confirmDelete(table: string) {
+  this.selectedTable = table;
+  this.showDeleteModal = true;
+  }
+
+cancelDelete() {
+  this.selectedTable = null;
+  this.showDeleteModal = false;
+}
+
+deleteTable() {
+  this.isDeleting = true;
+
+  setTimeout(() => {
+    this.http.post<Result>(this.baseUrl + '/tableController/deleteTable', { tableName: this.selectedTable }).subscribe({
+      next: (res) => {
+        this.isDeleting = false;
+        this.showDeleteModal = false;
+        this.fetchTables(); 
+      },
+      error: (err) => {
+        this.isDeleting = false;
+        console.error('Error deleting table:', err);
+      }
+    });
+  }, 2000);
 }
 
 }
